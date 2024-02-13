12845b453c6d8df203975f7eaf5b174d24aa27b8
Caleb Theil <86980706+ctheil@users.noreply.github.com>
2024-02-13 16:46:51 -0700

    ADDS: key stored in local storage

--- src/components/errors/ErrorBoundary.tsx
+++ src/components/errors/ErrorBoundary.tsx
@@ -14,6 +14,7 @@ class ErrorBoundary extends Component<Props, State> {
     hasError: false,
   };
   public static getDerivedStateFromError(_: Error): State {
+    console.warn(_);
     return { hasError: true };
   }
 
