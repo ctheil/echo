55348215f474180a3a879064b6b0460939d7e823
Caleb Theil <86980706+ctheil@users.noreply.github.com>
2024-02-20 13:14:08 -0700

    ADDS: threshold explanations to Settings
    As a user changes the threshold explanations for how that threshold will
    effect the prompt appear beneath the slider.
    
    This should introduce transparency into the working of echo and provide
    a better, more customizable experience for users.

--- src/components/echo/Settings.tsx
+++ src/components/echo/Settings.tsx
@@ -34,6 +34,20 @@ const marks = [
   },
 ];
 
+const thresholdExplanations: { [key: number]: string } = {
+  0: "The lowest threshold. With this low of a threshold all suggestions, regardless of their estimated impact on the promt will be visible.",
+  10: "With a threshold between 10 and 30, most suggestions will appear and have a nominal effect on the prompt",
+  20: "With a threshold between 10 and 30, most suggestions will appear and have a nominal effect on the prompt",
+  30: "With a threshold between 10 and 30, most suggestions will appear and have a nominal effect on the prompt",
+  40: "A threshold between 40 and 60 will filter out the unneeded suggestions and only target those that have a greater effect on the prompt.",
+  50: "A threshold between 40 and 60 will filter out the unneeded suggestions and only target those that have a greater effect on the prompt.",
+  60: "A threshold between 40 and 60 will filter out the unneeded suggestions and only target those that have a greater effect on the prompt.",
+  70: "A high threshold between 70 and 90 will only show suggestions that have a major impact on the prompt, giving you only needed suggestions in vague or ambigous areas of the original prompt.",
+  80: "A high threshold between 70 and 90 will only show suggestions that have a major impact on the prompt, giving you only needed suggestions in vague or ambigous areas of the original prompt.",
+  90: "A high threshold between 70 and 90 will only show suggestions that have a major impact on the prompt, giving you only needed suggestions in vague or ambigous areas of the original prompt.",
+  100: "A max threshold will rarely show suggestions unless an area of the prompt is incredibly vague and requires clarification.",
+};
+
 type Props = {
   setThreshold: (val: number) => void;
   threshold: number;
@@ -42,9 +56,8 @@ type Props = {
 const Settings = ({ setThreshold, threshold }: Props) => {
   const [open, setOpen] = useState(false);
 
-  const handleSliderChange = ({ target }: { target: { value: number } }) => {
-    console.log(target.value);
-    setThreshold(target.value);
+  const handleSliderChange = (_: Event, value: number | number[]) => {
+    setThreshold(value as number);
   };
 
   return (
@@ -63,11 +76,10 @@ const Settings = ({ setThreshold, threshold }: Props) => {
               <Box sx={{ marginTop: "1rem" }}>
                 <Slider
                   aria-label="Temperature"
-                  defaultValue={threshold}
+                  defaultValue={50}
+                  value={threshold}
                   onChange={handleSliderChange}
-                  // getAriaValueText={valuetext}
                   valueLabelDisplay="auto"
-                  shiftStep={10}
                   step={10}
                   marks={marks}
                   min={0}
@@ -75,6 +87,12 @@ const Settings = ({ setThreshold, threshold }: Props) => {
                   color="secondary"
                 />
               </Box>
+              <Typography variant="h6" color="secondary">
+                {threshold}
+              </Typography>
+              <Typography variant="body2">
+                {thresholdExplanations[threshold]}
+              </Typography>
             </Box>
           </DialogContent>
         </Dialog>
