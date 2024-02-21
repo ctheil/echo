f638e55a591d9817f391c3d883fd303c62baa1ce
Caleb Theil <86980706+ctheil@users.noreply.github.com>
2024-02-20 18:21:02 -0700

    ADDS: api key warning
    Adds a warning to the api key input modal warning users that their key
    will be stored in the browser's local storage and that the site should
    be used in private mode or with extensions that they trust.

--- src/components/KeyInputModal.tsx
+++ src/components/KeyInputModal.tsx
@@ -1,69 +1,121 @@
-import React, {  useState } from "react"
-import { Box, Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Link, TextField, ThemeProvider } from "@mui/material"
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Link,
+  TextField,
+  ThemeProvider,
+} from "@mui/material";
 import Storage from "../util/key";
 import { theme } from "./muiPalette";
 import { testKey } from "../util/openAi";
 
 export const KeyInputDialog = () => {
-    const [error, setError] = useState<string | null>(null);
-    const [loading, setLoading] = useState(false)
-    const [open, setOpen] = useState(!Storage.get("key"))
-    
-        return <ThemeProvider theme={theme}>
-        <Dialog 
-        open={ open } 
-        onClose={()=> {return}}
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(!Storage.get("key"));
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Dialog
+        open={open}
+        onClose={() => {
+          return;
+        }}
         PaperProps={{
-        component:"form",
-        onSubmit: async (event: React.FormEvent<HTMLFormElement>) => {
-            setLoading(true)
-                event.preventDefault(); 
-                const formData = new FormData(event.currentTarget); 
-                const formJSON = Object.fromEntries((formData as FormData).entries());
-                const key = formJSON.key as string;
-                console.log(key)
-                // NOTE: test key
-                const goodKey = await testKey(key);
-                setLoading(false)
-                if (goodKey instanceof Error) {
-                    return setError("Invalid api key")
-                    }
-                Storage.put("key", key)
-                setOpen(false)
+          component: "form",
+          onSubmit: async (event: React.FormEvent<HTMLFormElement>) => {
+            setLoading(true);
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJSON = Object.fromEntries(
+              (formData as FormData).entries(),
+            );
+            const key = formJSON.key as string;
+            console.log(key);
+            // NOTE: test key
+            const goodKey = await testKey(key);
+            setLoading(false);
+            if (goodKey instanceof Error) {
+              return setError("Invalid api key");
             }
-
+            Storage.put("key", key);
+            setOpen(false);
+          },
+        }}
+      >
+        <DialogTitle>OpenAI Api Key</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            In order to interact with this feature, you must first garner an{" "}
+            <Link
+              target="_blank"
+              color="secondary"
+              href="https://openai.com/blog/openai-api"
+            >
+              api key from OpenAI
+            </Link>{" "}
+            to interact with the ChatGPT model.{" "}
+            <Link
+              target="_blank"
+              color="secondary"
+              href="https://help.openai.com/en/articles/4936850-where-do-i-find-my-api-key"
+            >
+              Need help finding you key?
+            </Link>
+          </DialogContentText>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="key"
+            name="key"
+            label="OpenAI Api Key"
+            type="password"
+            fullWidth
+            variant="standard"
+            error={!!error}
+            helperText={error && "Invalid api key"}
+          />
+          <DialogContentText sx={{ marginTop: "1rem" }}>
+            <span style={{ color: "#dd524c" }}>Warning</span>: This key will be
+            stored in the browser&apos;s local storage.
+            <br />
+            <strong style={{ color: "#dd524c" }}>
+              Do not store your key in a public computer
+            </strong>{" "}
+            and only use in a brower with extensions you trust!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="outlined" color="secondary" type="submit">
+            Submit
+          </Button>
+        </DialogActions>
+        {loading && (
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              backgroundColor: "rgba(0, 0, 0, 0.4)",
+              justifyContent: "center",
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
             }}
-        >
-        
-            <DialogTitle>OpenAI Api Key</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                    In order to interact with this feature, you must first garner an <Link target="_blank" color="secondary" href="https://openai.com/blog/openai-api">api key from OpenAI</Link> to interact with the ChatGPT model. <Link target="_blank" color="secondary" href="https://help.openai.com/en/articles/4936850-where-do-i-find-my-api-key">Need help finding you key?</Link>
-                    </DialogContentText>
-                    <TextField 
-                    autoFocus
-                    required
-                    margin="dense"
-                    id="key"
-                    name="key"
-                    label="OpenAI Api Key"
-                    type="password"
-                    fullWidth
-                    variant="standard"
-                    error={!!error}
-                    helperText={error && "Invalid api key"}
-                    />
-                </DialogContent>
-            <DialogActions>
-                <Button variant="outlined" color="secondary" type="submit">Submit</Button>
-                </DialogActions>
-                {loading && 
-                    <Box sx={{display: "flex", alignItems: "center", backgroundColor: "rgba(0, 0, 0, 0.4)", justifyContent: "center", position: "absolute", top: 0, left: 0, right: 0, bottom: 0}}>
-                    <CircularProgress  color="secondary"/>
-                    </Box>
-                }
-            </Dialog>
-        </ThemeProvider>
-    }
-
-
+          >
+            <CircularProgress color="secondary" />
+          </Box>
+        )}
+      </Dialog>
+    </ThemeProvider>
+  );
+};
